584f2b8938a71c8d69634b4c544c74a1b85cd99c
Longbowou <blandedaniel@gmail.com>
2020-09-17 01:15:44 +0000

    [update] service orders datatable !

--- src/view/pages/user/orders/Datatable.vue
+++ src/view/pages/user/orders/Datatable.vue
@@ -67,7 +67,7 @@ import { SET_BREADCRUMB } from "@/core/services/store/modules/breadcrumbs.module
 import { SET_HEAD_TITLE } from "@/core/services/store/modules/htmlhead.module";
 import { mapGetters } from "vuex";
 import "@/assets/plugins/datatable/datatables.bundle";
-import { servicePurchasesUrl } from "@/core/datatables/urls";
+import { serviceOrdersUrl } from "@/core/datatables/urls";
 import JwtService from "@/core/services/jwt.service";
 import i18nService from "@/core/services/i18n.service";
 
@@ -116,7 +116,7 @@ export default {
       serverSide: true,
       stateSave: true,
       ajax: {
-        url: servicePurchasesUrl,
+        url: serviceOrdersUrl,
         headers: {
           "Accept-Language": i18nService.getActiveLanguage(),
           Authorization: "JWT " + JwtService.getAuth().token
