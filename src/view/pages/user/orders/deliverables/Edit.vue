2f9bafb9bc5dfc584e413acf6b6e934e217bc57c
Longbowou <blandedaniel@gmail.com>
2020-09-18 14:34:04 +0000

    [update] deliverable file uploads !

--- src/view/pages/user/orders/deliverables/Edit.vue
+++ src/view/pages/user/orders/deliverables/Edit.vue
@@ -23,6 +23,62 @@
       <div class="row justify-content-center">
         <deliverable-form :deliverable-id="$route.params.idDeliverable" />
       </div>
+
+      <div>
+        <hr class="mt-5" />
+
+        <div class="form-group row">
+          <label class="col-lg-3 col-form-label text-lg-right"
+            >Upload Files:</label
+          >
+          <div class="col-lg-9">
+            <div class="dropzone dropzone-multi" id="kt_dropzone_5">
+              <div class="dropzone-panel mb-lg-0 mb-2">
+                <a
+                  class="dropzone-select btn btn-light-primary font-weight-bold btn-sm"
+                  >Attach files</a
+                >
+              </div>
+
+              <div class="dropzone-items">
+                <div class="dropzone-item" style="display:none">
+                  <div class="dropzone-file">
+                    <div
+                      class="dropzone-filename"
+                      title="some_image_file_name.jpg"
+                    >
+                      <span data-dz-name>some_image_file_name.jpg</span>
+                      <strong>(<span data-dz-size>340kb</span>)</strong>
+                    </div>
+                    <div class="dropzone-error" data-dz-errormessage></div>
+                  </div>
+                  <div class="dropzone-progress">
+                    <div class="progress">
+                      <div
+                        class="progress-bar bg-primary"
+                        role="progressbar"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        aria-valuenow="0"
+                        data-dz-uploadprogress
+                      ></div>
+                    </div>
+                  </div>
+                  <div class="dropzone-toolbar">
+                    <span class="dropzone-delete" data-dz-remove
+                      ><i class="flaticon2-cross"></i
+                    ></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <span class="form-text text-muted"
+              >Max file size is <strong>1GB</strong> and max number of files is
+              <strong>10</strong>.</span
+            >
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
