448ed82375d296b7a5cd8debdfe2ada1c58288d8
Longbowou <blandedaniel@gmail.com>
2020-11-09 23:42:07 +0000

    [update] translation !

--- src/core/plugins/vue-i18n.js
+++ src/core/plugins/vue-i18n.js
@@ -4,6 +4,7 @@ import VueI18n from "vue-i18n";
 // Localisation language list
 import fr from "@/core/config/i18n/fr.json";
 import en from "@/core/config/i18n/en.json";
+import i18nService from "@/core/services/i18n.service";
 
 Vue.use(VueI18n);
 
@@ -11,7 +12,7 @@ let messages = {};
 messages = { ...messages, fr, en };
 
 // get current selected language
-const lang = localStorage.getItem("language") || "en";
+const lang = i18nService.getActiveLanguage();
 
 // Create VueI18n instance with options
 const i18n = new VueI18n({
