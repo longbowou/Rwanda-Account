169d36c6aa2346eca8f90aec73f0f13c871a18fd
Longbowou <blandedaniel@gmail.com>
2020-12-06 02:26:07 +0000

    [refactor] !

--- src/graphql/service-options-queries.js
+++ src/graphql/service-options-queries.js
@@ -1,15 +1,6 @@
 import gql from "graphql-tag";
 import { serviceOptionFields } from "@/graphql/fragments/service-option";
 
-export const queryServiceOptions = gql`
-  query {
-    serviceOptions {
-      ...serviceOptionFields
-    }
-  }
-  ${serviceOptionFields}
-`;
-
 export const queryServiceOption = gql`
   query serviceOption($id: UUID!) {
     serviceOption(id: $id) {
