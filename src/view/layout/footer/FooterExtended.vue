9c35929fceb97b0f9ff13988bf9a6883bbd43bfe
Longbowou <blandedaniel@gmail.com>
2024-02-07 17:39:33 -0600

    [Update] configs for demo deploy !

--- src/view/layout/footer/FooterExtended.vue
+++ src/view/layout/footer/FooterExtended.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- begin:: Footer -->
   <div
-    class="footer kt-grid__item bgi-position-center-center bgi-size-cover bgi-no-cover"
-    id="kt_footer"
-    style="background-image: url('media/bg/bg-mdtaf.png');"
+      class="footer kt-grid__item bgi-position-center-center bgi-size-cover bgi-no-cover"
+      id="kt_footer"
+      style="background-image: url('media/bg/bg-mdtaf.png');"
   >
     <!--begin::Container-->
     <div class="container py-lg-18 py-8">
@@ -13,10 +13,10 @@
         <div class="col-lg-4 my-lg-0 my-5">
           <h4 class="text-white pb-3">{{ brand }}</h4>
           <p
-            class="m-0 text-white opacity-45"
-            v-html="
+              class="m-0 text-white opacity-45"
+              v-html="
               $t(
-                'Product of Maison du Digital.<br />Platform for buying and selling digital services based in LOME TOGO.'
+                'Platform for buying and selling digital services.'
               )
             "
           ></p>
@@ -30,56 +30,56 @@
           <div class="d-flex align-items-center">
             <div class="d-flex flex-column mr-18">
               <router-link
-                to="/home"
-                v-slot="{ href, navigate, isActive, isExactActive }"
+                  to="/home"
+                  v-slot="{ href, navigate, isActive, isExactActive }"
               >
                 <a
-                  :href="href"
-                  class="text-white opacity-55 text-hover-primary"
-                  @click="navigate"
+                    :href="href"
+                    class="text-white opacity-55 text-hover-primary"
+                    @click="navigate"
                 >
                   {{ $t("Home") }}
                 </a>
               </router-link>
 
-              <router-link
-                to="/home"
-                v-slot="{ href, navigate, isActive, isExactActive }"
-              >
-                <a
-                  :href="href"
-                  class="text-white opacity-55 text-hover-primary"
-                  @click="navigate"
-                >
-                  {{ $t("Privacy") }}
-                </a>
-              </router-link>
+              <!--              <router-link-->
+              <!--                  to="/home"-->
+              <!--                  v-slot="{ href, navigate, isActive, isExactActive }"-->
+              <!--              >-->
+              <!--                <a-->
+              <!--                    :href="href"-->
+              <!--                    class="text-white opacity-55 text-hover-primary"-->
+              <!--                    @click="navigate"-->
+              <!--                >-->
+              <!--                  {{ $t("Privacy") }}-->
+              <!--                </a>-->
+              <!--              </router-link>-->
 
-              <router-link
-                to="/home"
-                v-slot="{ href, navigate, isActive, isExactActive }"
-              >
-                <a
-                  :href="href"
-                  class="text-white opacity-55 text-hover-primary"
-                  @click="navigate"
-                >
-                  {{ $t("Terms & Conditions") }}
-                </a>
-              </router-link>
+              <!--              <router-link-->
+              <!--                  to="/home"-->
+              <!--                  v-slot="{ href, navigate, isActive, isExactActive }"-->
+              <!--              >-->
+              <!--                <a-->
+              <!--                    :href="href"-->
+              <!--                    class="text-white opacity-55 text-hover-primary"-->
+              <!--                    @click="navigate"-->
+              <!--                >-->
+              <!--                  {{ $t("Terms & Conditions") }}-->
+              <!--                </a>-->
+              <!--              </router-link>-->
 
-              <router-link
-                to="/home"
-                v-slot="{ href, navigate, isActive, isExactActive }"
-              >
-                <a
-                  :href="href"
-                  class="text-white opacity-55 text-hover-primary"
-                  @click="navigate"
-                >
-                  {{ $t("Sitemap") }}
-                </a>
-              </router-link>
+              <!--              <router-link-->
+              <!--                  to="/home"-->
+              <!--                  v-slot="{ href, navigate, isActive, isExactActive }"-->
+              <!--              >-->
+              <!--                <a-->
+              <!--                    :href="href"-->
+              <!--                    class="text-white opacity-55 text-hover-primary"-->
+              <!--                    @click="navigate"-->
+              <!--                >-->
+              <!--                  {{ $t("Sitemap") }}-->
+              <!--                </a>-->
+              <!--              </router-link>-->
             </div>
 
             <div class="d-flex flex-column"></div>
@@ -94,15 +94,15 @@
           <form class="rounded" style="background-color: rgba(0,0,0,.2);">
             <div class="input-group p-2 align-items-center">
               <input
-                type="text"
-                class="form-control rounded-right border-0 bg-transparent text-white opacity-80"
-                placeholder="Enter Your Email"
+                  type="text"
+                  class="form-control rounded-right border-0 bg-transparent text-white opacity-80"
+                  placeholder="Enter Your Email"
               />
               <div class="input-group-append p-0 mr-1">
                 <button
-                  class="btn btn-fh btn-sm btn-primary px-6 rounded-left"
-                  style="z-index: 1"
-                  type="button"
+                    class="btn btn-fh btn-sm btn-primary px-6 rounded-left"
+                    style="z-index: 1"
+                    type="button"
                 >
                   {{ $t("Join us") }}
                 </button>
@@ -121,18 +121,18 @@
     <!--begin::Container-->
     <div class="container py-8">
       <div
-        class="d-flex align-items-center justify-content-between flex-lg-row flex-column"
+          class="d-flex align-items-center justify-content-between flex-lg-row flex-column"
       >
         <!--begin::Copyright-->
         <div class="d-flex align-items-center order-lg-1 order-2">
           <img
-            alt="Logo"
-            src="media/logos/logo.png"
-            class="logo-sticky max-h-35px"
+              alt="Logo"
+              src="media/logos/logo.png"
+              class="logo-sticky max-h-35px"
           />
           <span class="text-muted font-weight-bold mx-2">2020 ©</span>
-          <a href="#" class="text-primary text-hover-primary">
-            Maison du Digital
+          <a href="https://gitlab.com/longbowou" class="text-primary text-hover-primary">
+            {{ brand }}
           </a>
         </div>
         <!--end::Copyright-->
@@ -164,8 +164,8 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { BRAND } from "@/core/config/app";
+import {mapGetters} from "vuex";
+import {BRAND} from "@/core/config/app";
 
 export default {
   name: "KTFooter",
